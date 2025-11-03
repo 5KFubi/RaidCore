b25b80f9198dafe77d3f3a67789433ab6e542614
5KFubi <5kelefubi@gmail.com>
2025-11-03 22:42:27 +0200

    1.0

--- src/main/java/me/fivekfubi/PROVIDER_RaidCore.java
+++ src/main/java/me/fivekfubi/PROVIDER_RaidCore.java
@@ -1,16 +1,18 @@
 package me.fivekfubi;
 
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Map;
-import java.util.function.Consumer;
+import java.util.List;
 
 public interface PROVIDER_RaidCore {
     void test();
     void register(JavaPlugin plugin);
 
-    Map<JavaPlugin, Map<String, Consumer<Object>>> get_registry();
-    void add_function(JavaPlugin plugin, String key, Consumer<Object> function);
-    Consumer<Object> get_function(JavaPlugin plugin, String key);
-    void call_function(JavaPlugin plugin, String key, Object data);
+    List<ItemStack> get_item(JavaPlugin plugin, String path, int amount, HOLDER holder_data);
+
+    //Map<JavaPlugin, Map<String, Consumer<Object[]>>> get_registry();
+    //void add_function(JavaPlugin plugin, String key, Consumer<Object[]> function);
+    //Consumer<Object[]> get_function(JavaPlugin plugin, String key);
+    //void call_function(JavaPlugin plugin, String key, Object... args);
 }
