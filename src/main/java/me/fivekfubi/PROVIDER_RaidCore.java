95b3ad12cf2f350ff5e45b2ec34f17db91a77ff1
5KFubi <5kelefubi@gmail.com>
2025-11-03 15:40:10 +0200

    Test 1

--- src/main/java/me/fivekfubi/PROVIDER_RaidCore.java
+++ src/main/java/me/fivekfubi/PROVIDER_RaidCore.java
@@ -2,7 +2,15 @@ package me.fivekfubi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
+import java.util.function.Consumer;
+
 public interface PROVIDER_RaidCore {
     void test();
     void register(JavaPlugin plugin);
+
+    Map<JavaPlugin, Map<String, Consumer<Object>>> get_registry();
+    void add_function(JavaPlugin plugin, String key, Consumer<Object> function);
+    Consumer<Object> get_function(JavaPlugin plugin, String key);
+    void call_function(JavaPlugin plugin, String key, Object data);
 }
